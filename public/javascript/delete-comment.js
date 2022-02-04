a21fbd6c45a4ab6a7efef2b182c53231b7d366fb
Bennet Wilson <bennetwilson@gmail.com>
2022-02-03 18:45:08 -0700

    deleting redundant code

--- public/javascript/delete-comment.js
+++ public/javascript/delete-comment.js
@@ -1,5 +1,4 @@
 const deleteCommentClickHandler = async (event) => {
-    alert('This sucks')
     const comment_id = event.target.getAttribute('data-id')
       await fetch(`/api/comments/${comment_id}`, {
         method: 'DELETE'
